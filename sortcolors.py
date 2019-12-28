1e5a65a06b55307fbf0ecff859839057d79a6df3
yuehhan <51277751+yuehhan@users.noreply.github.com>
2019-12-27 22:07:38 -0500

    Add files via upload

---
+++ sortcolors.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left, right, zero = 0, len(nums)-1, 0
+        while left <= right:
+            if nums[left] == 0:
+                nums[left], nums[zero] = nums[zero], nums[left]
+                left += 1
+                zero += 1
+            elif nums[left] == 2:
+                nums[left], nums[right] = nums[right], nums[left]
+                right -= 1
+            else:
+                left += 1
+        return nums
+        
