262eaa427057ee4c73dfec552f0d1f87bcb4aa0b
yuehhan <51277751+yuehhan@users.noreply.github.com>
2020-02-29 14:32:03 -0500

    First Backtracking problem solved!

---
+++ generate_parenthesis.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n == 0: return []
+        res = []
+        open = n
+        close = n
+        string = ""
+        self.generate(res, open, close, string)
+        return res
+    
+    def generate(self, res, open, close, string):
+        if open > close or open < 0 or close < 0:
+            return
+        if open == 0 and close == 0:
+            res.append(string)
+            return
+        
+        self.generate(res, open-1, close, string+'(')
+        self.generate(res, open, close-1, string+')')
