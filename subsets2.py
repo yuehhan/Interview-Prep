9effb3ef785d96bc04341dc443ece3221ac3ff6f
yuehhan <51277751+yuehhan@users.noreply.github.com>
2020-03-03 15:27:06 -0500

    Add files via upload

---
+++ subsets2.py
@@ -0,0 +1,20 @@
+# Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
+
+# Note: The solution set must not contain duplicate subsets.
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        current = []
+        index = 0
+        self.generate(index, nums, current, res)
+        return res
+
+    
+    def generate(self, index, nums, current, res):
+        if current not in res:
+            res.append(current)
+            
+        for i in range(index, len(nums)):
+            self.generate(i+1, nums, current+[nums[i]], res)
+        
